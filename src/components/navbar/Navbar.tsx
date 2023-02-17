3c68427222b995ad93c695bfc134da0d157a4aa2
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2023-02-17 18:41:34 -0300

    ci: :package: Typescript and scss was added and checked.

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+      <div className="App">
+      <button className="button">Haz clic aquí</button>
+      </div>
+    );
+  }
+  export default Navbar;
